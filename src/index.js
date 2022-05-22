8e4ab68e9f79a44ad7628b2a9d756bae10fc4597
Jerichonina <102739349+jerichonina@users.noreply.github.com>
2022-05-22 11:56:42 +0800

    edited the function of weather forecast API

--- src/index.js
+++ src/index.js
@@ -38,7 +38,8 @@ let currentTime = new Date();
 li.innerHTML = formatDate(currentTime);
 
 // Display weather forecast
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`; 
@@ -103,6 +104,7 @@ function defaultCity (city) {
    console.log(coordinates);
    let apiKey = "fc81915063c1c948e13c1b9f6ba1e112";
    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+   console.log(apiUrl);
    axios.get(apiUrl).then(displayForecast);
  }
 
@@ -145,4 +147,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsius);
 
 defaultCity ("Hong Kong");
-displayForecast();
